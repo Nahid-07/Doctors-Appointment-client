73ea6440ecd69a895f964ad078ea529030b20c5e
Nahid-07 <nahidasif1998@gmail.com>
2023-06-12 22:48:20 +0600

    tanstack query added

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "react-day-picker/dist/style.css";
 import ContextProvider from "./ContextApi/ContextProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <QueryClientProvider client={QueryClient}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 reportWebVitals();

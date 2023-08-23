c4ce6e34514d4c146ae89e34a9b39725b4b181a3
Nahid-07 <nahidasif1998@gmail.com>
2023-08-23 13:22:58 +0600

    jwt token set on the localstorage and send to the backend for verify

--- src/pages/Signup/SignUp.js
+++ src/pages/Signup/SignUp.js
@@ -62,7 +62,6 @@ const SignUp = () => {
   const getUserToken = email =>{
     fetch(`http://localhost:5000/jwt?email=${email}`)
     .then(res => res.json()).then(data => {
-      console.log(data);
       if(data.accessToken){
         localStorage.setItem('accessToken', data.accessToken)
       }

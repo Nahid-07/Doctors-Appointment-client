d27cbca49a738ffa28238c730f25edad2ce6a446
Nahid-07 <nahidasif1998@gmail.com>
2023-09-04 12:14:24 +0600

    add doctor form added

--- src/pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,25 +1,47 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddDoctor = () => {
+  const {handleSubmit, register} = useForm()
+  const doctorData = (data)=>{
+    console.log(data);
+  }
   return (
     <div className="flex justify-center">
-
-      <form action="" className="border p-5 md:p-20 shadow-md rounded-lg mt-40">
+      <form onSubmit={handleSubmit(doctorData)} className="border p-5 md:p-20 shadow-md rounded-lg mt-40">
         <div>
-          <label htmlFor="" className="text-lg font-semibold text-gray-700">Name</label> <br />
+          <label htmlFor="" className="text-lg font-semibold text-gray-700">
+            Name
+          </label>{" "}
+          <br />
           <input
+            {...register("doctorName")}
             type="text"
-            placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="my-5">
-          <label htmlFor="" className="text-lg font-semibold text-gray-700">Email</label> <br />
+          <label htmlFor="" className="text-lg font-semibold text-gray-700">
+            Email
+          </label>{" "}
+          <br />
           <input
-            type="text"
-            placeholder="Type here"
+          {...register("doctorEmail")}
+            type="email"
             className="input input-bordered w-full max-w-xs"
           />
+        </div>
+        <div>
+          <select {...register("speciality")} className="select select-bordered w-full max-w-xs mb-5">
+            <option value="Teeth Cleaning">
+            Teeth Cleaning
+            </option>
+            <option value="Cosmetic Dentistry">Cosmetic Dentistry</option>
+            <option value="Cavity Protection">Cavity Protection</option>
+            <option value="Teeth Orthodontics">Teeth Orthodontics</option>
+            <option value="Oral Surgery">Oral Surgery</option>
+            <option value="Pediatric Dental">Pediatric Dental</option>
+          </select>
         </div>
         <div>
           <input
@@ -27,6 +49,9 @@ const AddDoctor = () => {
             className="file-input file-input-bordered w-full max-w-xs"
           />
         </div>
+        <button className="bg-[#3A4256] w-full py-3 rounded-md text-white font-medium mt-5">
+          Add Doctor
+        </button>
       </form>
     </div>
   );

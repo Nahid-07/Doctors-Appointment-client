ca2f9ab3e30b1248a5764abe9a6c79ea3dfa0059
Nahid-07 <nahidasif1998@gmail.com>
2023-09-29 18:53:01 +0600

    doctor list added on dashbord

--- src/pages/Dashboard/AddDoctor/DoctorList.js
+++ src/pages/Dashboard/AddDoctor/DoctorList.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const DoctorList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { data: doctors } = useQuery({
+    queryKey: ["doctors"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/doctors");
+      const data = await res.json();
+      return data;
+    },
+  });
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Speciality</th>
+              <th>Email</th>
+              <th>Image</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {doctors.map((doctor,i) => (
+              <tr>
+                <th>{i+1}</th>
+                <td>{doctor.DoctorName}</td>
+                <td>{doctor.speciality}</td>
+                <td>{doctor.doctorEmail}</td>
+                <td><img src={doctor.image} alt="doctor image" width="20px" /></td>
+                <td><button className='btn btn-xs bg-red-500 text-white'>Delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default DoctorList;

08a6f1f6cf851c78196820b2a1c8f2724fabcc95
Nahid-07 <nahidasif1998@gmail.com>
2023-10-08 11:23:48 +0600

    delete method implemented

--- src/pages/Dashboard/AddDoctor/DoctorList.js
+++ src/pages/Dashboard/AddDoctor/DoctorList.js
@@ -1,8 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Speener from "../../../Speener/Speener";
 
 const DoctorList = () => {
-  const { data: doctors } = useQuery({
+  const {
+    data: doctors,
+    refetch,
+    isLoading,
+  } = useQuery({
     queryKey: ["doctors"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/doctors");
@@ -10,13 +15,30 @@ const DoctorList = () => {
       return data;
     },
   });
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/doctordelete/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const confirm = window.confirm('Are you sure you want to delete doctor?');
+        if(confirm){
+          if(data.deletedCount > 0){
+            refetch()
+          }
+        }
+      });
+  };
+  if (isLoading) {
+    return <Speener></Speener>;
+  }
   return (
     <div>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead>
-            <tr>
+            <tr className="text-lg">
               <th>No.</th>
               <th>Name</th>
               <th>Speciality</th>
@@ -26,14 +48,23 @@ const DoctorList = () => {
             </tr>
           </thead>
           <tbody>
-            {doctors.map((doctor,i) => (
-              <tr>
-                <th>{i+1}</th>
+            {doctors.map((doctor, i) => (
+              <tr className="text-lg">
+                <th>{i + 1}</th>
                 <td>{doctor.DoctorName}</td>
                 <td>{doctor.speciality}</td>
                 <td>{doctor.doctorEmail}</td>
-                <td><img src={doctor.image} alt="doctor image" width="20px" /></td>
-                <td><button className='btn btn-xs bg-red-500 text-white'>Delete</button></td>
+                <td>
+                  <img src={doctor.image} alt="doctor" width="50px" />
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(doctor._id)}
+                    className="btn btn-xs bg-red-500 text-white"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
